4ac4c0973e38f9be2e3bfea6bdf0fceadfdb9726
anfeespi <122488352+anfeespi@users.noreply.github.com>
2025-05-08 23:34:41 -0500

    Create UVa 459.cpp

---
+++ UVa 459.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i, a, b) \
+    for (int i = int(a); i <= int(b); i++)
+    
+vector<int> pset(1000);
+
+void initSet(int _size) { 
+    pset.resize(_size); REP (i, 0, _size - 1) pset[i] = i; 
+}
+
+int findSet(int i) { 
+    return (pset[i] == i) ? i : (pset[i] = findSet(pset[i])); 
+}
+
+void unionSet(int i, int j) {
+    pset[findSet(i)] = findSet(j);
+}
+
+bool isSameSet(int i, int j) { 
+    return findSet(i) == findSet(j); 
+}
+
+int numberOfSets(){
+    bitset<26> visited;
+    
+    for(int i = 0; i < pset.size(); i++){
+        visited[findSet(i)] = true;
+    }
+    
+    return visited.count();
+}
+
+int main(){
+    int t, tam;
+    char c;
+    string aux;
+    cin>>t;
+    while(t--){
+        cin>>c;
+        tam = c - 'A' + 1;
+        cin.ignore();
+        
+        initSet(tam);
+        
+        while(getline(cin, aux)){
+            if(aux == "") break;
+            unionSet(aux[0] - 'A', aux[1] - 'A');
+        }
+        
+        cout<<numberOfSets()<<endl;
+        
+        
+        if(t) cout<<endl;
+    }
+}
